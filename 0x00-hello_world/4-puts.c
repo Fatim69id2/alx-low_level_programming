162aa177ed68d91c045267e6b118ade59e74d3ed
Fatim69id2 <fatimazahrajul@gmail.com>
2023-06-15 11:14:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ * description: it's my first languge
+ * * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
