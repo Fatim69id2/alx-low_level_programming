e47b3260a276243bcde0b348c2d0595fabb00b06
Fatim69id2 <fatimazahrajul@gmail.com>
2023-06-15 10:38:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
- puts("Programming is like building a multilingual puzzle");
+ puts("\"Programming is like building a multilingual puzzle");
  return (0);
 }
