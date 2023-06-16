c16e8100f3609810c58b79060a8468c9df284186
Fatim69id2 <fatimazahrajul@gmail.com>
2023-06-16 11:42:38 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if ((letter != 'q') && letter != 'e')
+			putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
