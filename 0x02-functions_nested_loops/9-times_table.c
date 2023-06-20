0862f38639d8d68aaff1fa03f11f1d90a1a79c72
Fatim69id2 <fatimazahrajul@gmail.com>
2023-06-20 11:10:18 -0700

    5-sign.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,20 +6,33 @@
 */
 void times_table(void)
 {
-	int a, b, product;
+	int a, b, c, d, e;
 
 	for (a = 0; a <= 9; a++)
 	{
 	for (b = 0; b <= 9; b++)
 	{
-		product = a * b;
-
-	if (b == 0)
-		printf("%2d", product);
-	else
-		printf(", %2d", product);
+	a * b;
+	if (c > 9)
+	{
+	d = c % 10;
+	e = (c -d) / 10;
+	_putchar(44);
+	_putchar(32);
+	_putchar(e + '0');
+	_putchar(d + '0');
 	}
-
-	putchar('\n');
+	else
+	{
+	if (b != 0)
+	{
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
+	}
+	_putchar(c + '0');
+	}
+	}
+	_putchar('\n');
 	}
 }
