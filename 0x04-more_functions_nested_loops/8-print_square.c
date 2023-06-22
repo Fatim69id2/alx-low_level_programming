298779fbe1c22684a30feed715173519ee0b0607
Fatim69id2 <fatimazahrajul@gmail.com>
2023-06-22 08:29:58 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - Prints n squares according n number of times
+ * @size: The number of squares/number of times
+ * Return: empty
+*/
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (a = 0; a < size; a++)
+	{
+	for (b = 0; b < size; b++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
+}
