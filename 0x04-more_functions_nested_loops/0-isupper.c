8569b55e40a035b822ef8167c77b704603a838b1
Fatim69id2 <fatimazahrajul@gmail.com>
2023-06-22 04:52:01 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,9 +4,9 @@
  *@x: The number to be checked
  *Return: 1 for upper letter or 0 for any else
 */
-int _isupper(int c)
+int _isupper(int x)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (x >= 65 && x <= 90)
 	{
 	return (1);
 	}
