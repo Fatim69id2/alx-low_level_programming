fac6faa6fa0499a634af5520b34a7ebb7d14c3b9
Fatim69id2 <fatimazahrajul@gmail.com>
2023-06-22 11:17:01 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - Prints an integer
+ * @n: The integer to prints
+ * Return: Nothing!
+*/
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+	_putchar('-');
+	n = -n;
+	}
+	if (n / 10 != 0)
+	{
+	print_number(n / 10);
+	}
+	_putchar('0' + n % 10);
+}
