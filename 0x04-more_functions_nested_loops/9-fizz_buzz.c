a2caf05b254708b4b82f10694cb10f080fa101a8
Fatim69id2 <fatimazahrajul@gmail.com>
2023-06-24 06:22:53 -0700

    ;

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,18 +13,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else if (i % 15 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf("FizzBuzz ");
-		else if (i == 1)
-			printf("%d", i);
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
 		else
-			printf(" %d", i);
-	}
-
+			printf("%d ", i);
+		}
+	{
 	printf("\n");
 	return (0);
 }
