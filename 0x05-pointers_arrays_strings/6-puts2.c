294a47f156f31b9cf2df2ef48daa807ca0a245cf
Fatim69id2 <fatimazahrajul@gmail.com>
2023-06-26 18:12:10 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,15 +8,11 @@
 void puts2(char *str)
 {
 	int i = 0;
-	int j;
 
 	while (str[i] != '\0')
 	{
-		i++;
+		putchar(str[i]);
+		i += 2;
 	}
-	for (j = 0; j < i; j += 2)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+	putchar('\n');
 }
