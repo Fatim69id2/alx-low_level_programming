16ae8eea13eb49eb1267e4b2e04d4c8e42282d6e
Fatim69id2 <fatimazahrajul@gmail.com>
2023-06-29 04:54:29 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -19,11 +19,11 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 	src_len++;
 	}
-	for (index = 0; src[index++] && index < n; index++)
+	for (index = 0; index < n && src[index++]; index++)
 	{
 	dest[index] = src[index];
 	}
-	for (index = src_len; index < n; index++)
+	for (; index < n; index++)
 	{
 	dest[index] = '\0';
 	}
