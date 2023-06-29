1deff98c2cbf394b75477ddc1d8c568f5db38c88
Fatim69id2 <fatimazahrajul@gmail.com>
2023-06-29 12:42:08 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_string_toupper - Changes all lowercase letters
+ * string_toupper - Changes all lowercase letters
  * of a string to uppercase
  * @str: The string to be changed
  * Return: A pointer to the changed string
