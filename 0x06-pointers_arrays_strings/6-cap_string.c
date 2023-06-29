7bbcc9554deab11da8949afdaa5fa5fdb361f6af
Fatim69id2 <fatimazahrajul@gmail.com>
2023-06-29 16:55:23 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,9 +11,9 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-	while (!(str[i] >= 'a' && str[i] <= 'z'))
+	while (str[i] >= 'a' && str[i] <= 'z')
 	i++;
-	if (str[i] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+	if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
 	str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
 	str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
 	str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
