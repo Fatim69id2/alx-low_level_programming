eacbccbc49a5c0df6083a97b19a5a1dd447790b3
Fatim69id2 <fatimazahrajul@gmail.com>
2023-06-29 17:10:29 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,13 +11,13 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-	while (str[i] >= 'a' && str[i] <= 'z')
+	if (str[i] >= 'a' && str[i] <= 'z')
 	i++;
 	if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
-	str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
-	str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
-	str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
-	str[i - 1] == '}' || i == 0)
+			str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+			str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+			str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+			str[i - 1] == '}' || str[i - 1] == '-' || str[i - 1] == '\'')
 	str[i] -= 32;
 	i++;
 	}
