0cc6b17d5087b086cf7e92b2a8822e8e4195fdfb
Fatim69id2 <fatimazahrajul@gmail.com>
2023-07-07 10:50:51 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Prints the sum of args positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	unsigned int j, sum = 0;
+	char *s;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			s = argv[i];
+			for (j = 0; j < strlen(s); j++)
+			{
+				if (s[j] < 48 || s[j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(s);
+			s++;
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("O\n");
+	}
+	return (0);
+}
