a3ed9d1366326b83a243228772125d6b45a075f2
Fatim69id2 <fatimazahrajul@gmail.com>
2023-07-14 05:40:09 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,11 +11,16 @@
 char *create_array(unsigned int size, char c)
 {
 	char *s;
+	unsigned int i;
 
 	s = malloc(size * sizeof(c));
-	if (s == 0)
+	if (size == 0 || s == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
 	}
 	return (s);
 }
