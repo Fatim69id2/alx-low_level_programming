8400b037e0e56e6fd134f148e7a58171f5b23cbe
Fatim69id2 <fatimazahrajul@gmail.com>
2023-07-17 06:03:38 -0700

    2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - Entry point print the name
+*the file it was compiled from
+*Return: nothing to be returned
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
