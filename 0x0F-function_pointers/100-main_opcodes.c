f922360dcdc1f18bbb29c3e00bae930575f7c26e
Fatim69id2 <fatimazahrajul@gmail.com>
2023-07-19 07:04:49 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	char *opc = (char *) main;
+	int i, nb;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	nb = atoi(argv[1]);
+
+	if (nb < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < nb; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nb - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
