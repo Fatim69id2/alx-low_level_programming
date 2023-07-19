6fa2408af75a92892497b3e1bb9dccbd2c6338d3
Fatim69id2 <fatimazahrajul@gmail.com>
2023-07-19 06:27:57 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+*print_name - Prints a name
+*@name:Name to print
+*@f: Function pointer
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+
+	if (name == NULL || f == NULL)
+	return;
+
+	f(name);
+
+}
