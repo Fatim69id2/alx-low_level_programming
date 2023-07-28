a91cc928dd797a3868628274afac3fb681f6dc43
Fatim69id2 <fatimazahrajul@gmail.com>
2023-07-28 13:18:12 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,12 +6,10 @@
  * printMessageBeforeMain() so that it
  *   is executed before main()
 */
-void printMessageBeforeMain(void) __attribute__((constructor));
+void _constructor() __attribute__((constructor));
 
-void printMessageBeforeMain(void)
+void _constructor()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf(" I bore my house upon my back!\n");
-	printf("A tortoise, having pretty good sense of a hare's nature");
-	printf("challenges one to a race.\n");
 }
