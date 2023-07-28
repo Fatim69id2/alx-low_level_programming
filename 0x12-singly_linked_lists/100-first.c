4c024d165e58a8778da71638c8ccd48c52df87ac
Fatim69id2 <fatimazahrajul@gmail.com>
2023-07-28 13:27:39 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,8 +2,8 @@
 #include "lists.h"
 
 /**
- * printMessageBeforeMain - Apply the constructor attribute to
- * printMessageBeforeMain() so that it
+ * _constructor - Apply the constructor attribute to
+ * _constructor() so that it
  *   is executed before main()
 */
 void _constructor(void) __attribute__((constructor));
@@ -11,5 +11,5 @@ void _constructor(void) __attribute__((constructor));
 void _constructor(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf(" I bore my house upon my back!\n");
+	printf("I bore my house upon my back!\n");
 }
